0850da3afe2a83dfffd762bbb9a2784042682417
Jurica Migac <jurica.migac@ibmix.hr>
2023-02-13 10:54:00 +0100

    #5: Simple controller

--- src/main/java/hr/ibmix/githubactions/controllers/SimpleController.java
+++ src/main/java/hr/ibmix/githubactions/controllers/SimpleController.java
@@ -3,10 +3,10 @@ package hr.ibmix.githubactions.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Rest Controller
 public class SimpleController {
 
-    @GetMapping
+    @Get Mapping
     public String getSimpleContent() {
         return "Dev Minutes";
     }
